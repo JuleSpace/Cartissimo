dfd72c8eaa06ece8daca703af16b9df0c625df79
Julien-Jacques <julien.jacques@limayrac.fr>
2025-07-01 17:24:54 +0200

    correction index.js (model)

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,21 +1,22 @@
+const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 // Initialisation des modèles
-const User = require('./User')(sequelize);
-const Patient = require('./Patient')(sequelize);
-const Theme = require('./Theme')(sequelize);
-const Animation = require('./Animation')(sequelize);
-const UserTheme = require('./UserTheme')(sequelize);
-const patient_therapists = require('./PatientTherapist')(sequelize);
-const Orthophoniste = require('./Orthophoniste')(sequelize);
+const User = require('./User')(sequelize, DataTypes);
+const Patient = require('./Patient')(sequelize, DataTypes);
+const Theme = require('./Theme')(sequelize, DataTypes);
+const Animation = require('./Animation')(sequelize, DataTypes);
+const UserTheme = require('./UserTheme')(sequelize, DataTypes);
+const patient_therapists = require('./PatientTherapist')(sequelize, DataTypes);
+const Orthophoniste = require('./Orthophoniste')(sequelize, DataTypes);
 
 // Définition des associations
-User.associate({ User, Patient, Theme, Animation, UserTheme, patient_therapists });
-Patient.associate({ User, Patient, Theme, Animation, UserTheme, patient_therapists });
-Theme.associate({ User, Patient, Theme, Animation, UserTheme, patient_therapists });
-Animation.associate({ User, Patient, Theme, Animation, UserTheme, patient_therapists });
-UserTheme.associate({ User, Patient, Theme, Animation, UserTheme, patient_therapists });
-Orthophoniste.associate({ User, Patient, Theme, Animation, UserTheme, patient_therapists });
+User.associate({ User, Patient, Theme, Animation, UserTheme, patient_therapists, Orthophoniste });
+Patient.associate({ User, Patient, Theme, Animation, UserTheme, patient_therapists, Orthophoniste });
+Theme.associate({ User, Patient, Theme, Animation, UserTheme, patient_therapists, Orthophoniste });
+Animation.associate({ User, Patient, Theme, Animation, UserTheme, patient_therapists, Orthophoniste });
+UserTheme.associate({ User, Patient, Theme, Animation, UserTheme, patient_therapists, Orthophoniste });
+Orthophoniste.associate({ User, Patient, Theme, Animation, UserTheme, patient_therapists, Orthophoniste });
 
 module.exports = {
   sequelize,
@@ -26,4 +27,4 @@ module.exports = {
   UserTheme,
   patient_therapists,
   Orthophoniste
-}; 
+};
